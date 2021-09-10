e27d5a7c50a0e4289db8b2061e4ded770aefe16e
şeyma <seymasuzukk@gmail.com>
2021-09-10 11:50:19 +0300

    V4

--- src/projectvendingmachine/Runner.java
+++ src/projectvendingmachine/Runner.java
@@ -4,7 +4,13 @@ public class Runner {
     public static void main(String[] args) {
         Products product=new Products();
         Methods methods =new Methods();
-        methods.select(product);
+        System.out.println("merhaba alisverise hos geldiniz!...");
+        double money=methods.money();
+        System.out.println("hesabinizda " + money +"dolar var");
+        product.setPrice(methods.select(product));
+        money = methods.balance(product.getPrice(), money, product);
+        methods.purchase(product.getPrice(), money, product);
+
 
 
     }
